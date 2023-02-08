1e744f4e5dfbffbb5657c9cf4e60dc9a0eac4a50
Oh Ga Eun <dhrkdms4113@naver.com>
2023-02-08 17:45:27 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+function handleRequest(request, response) {
+
+  if (request.url === '/currenttime'){
+    response.statusCode = 200; 
+    response.end('<h1>' + new Date().toISOString() + '</h1>');
+  } else if (request.url === '/'){
+    response.statusCode = 200; 
+    response.end('<h1>Hello World</h1>');
+  } 
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000);    // (포트번호)
